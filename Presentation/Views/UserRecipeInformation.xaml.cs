0369207ecb171c1e5999b4cf452a668a75760855
reut <reuttmm@gmail.com>
2023-08-08 01:12:30 +0300

    create user recipe information view

---
+++ Presentation/Views/UserRecipeInformation.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Presentation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Presentation.Views
+{
+    /// <summary>
+    /// Interaction logic for UserRecipeInformation.xaml
+    /// </summary>
+    public partial class UserRecipeInformation : UserControl
+    {
+        public UserRecipeInformation()
+        {
+            InitializeComponent();
+            grid.DataContext = new SuccessData() { Images = new List<string> { "https://chef-lavan.co.il/wp-content/uploads/old-storage/uploads/images/cf2a5b4afd4c80bc67b190f87a5752f1.jpg", 
+                "https://chef-lavan.co.il/wp-content/uploads/old-storage/uploads/images/cf2a5b4afd4c80bc67b190f87a5752f1.jpg", 
+                "https://chef-lavan.co.il/wp-content/uploads/old-storage/uploads/images/cf2a5b4afd4c80bc67b190f87a5752f1.jpg", 
+                "https://chef-lavan.co.il/wp-content/uploads/old-storage/uploads/images/cf2a5b4afd4c80bc67b190f87a5752f1.jpg", 
+                "https://chef-lavan.co.il/wp-content/uploads/old-storage/uploads/images/cf2a5b4afd4c80bc67b190f87a5752f1.jpg" }, Comment = "", Rating = 4 };
+        }
+    }
+}
